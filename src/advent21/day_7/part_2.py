51965038abfa9e83090426c6912cf0859446c252
James Trimarco <james.trimarco@gmail.com>
2021-12-07 17:09:23 -0500

    solves part 2

--- src/advent21/day_7/part_2.py
+++ src/advent21/day_7/part_2.py
@@ -1,17 +1,22 @@
 import sys
 import numpy as np
 from nptyping import NDArray
-from pathlib import Path
 from advent21.utils import handle_input, Timer, read_ints_sep_comma
 
 
-def find_min_fuel(crabs: NDArray) -> int:
+def find_distances(crabs: NDArray) -> int:
     min = np.inf
     for index in range(crabs.size):
-        sum = np.sum(np.abs(crabs[index] - crabs))
+        distances = np.array(np.abs(crabs[index] - crabs), dtype=np.int32)
+        partial_sums = [partial_sum(x) for x in distances]
+        sum = np.sum(partial_sums)
         if sum < min:
             min = sum
-    return min
+    return int(min)
+
+
+def partial_sum(nth_partial: int) -> int:
+    return int((nth_partial * (nth_partial + 1)) / 2)
 
 
 if __name__ == "__main__":
@@ -22,6 +27,7 @@ if __name__ == "__main__":
     data_path = handle_input(day_number, sys.argv[1])
     crabs = read_ints_sep_comma(data_path)
     crabs = np.asarray(crabs, dtype=np.int32)
-    print(find_min_fuel(crabs))
+    minimum_fuel = find_distances(crabs)
+    print(minimum_fuel)
 
     t.stop()
