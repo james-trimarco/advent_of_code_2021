6c4448c5d64cb44d3f026e9e1a7009871f31178a
James Trimarco <james.trimarco@gmail.com>
2021-12-06 20:40:53 -0500

    first stab at day 1 (not working)

--- src/advent21/day_6/part_1.py
+++ src/advent21/day_6/part_1.py
@@ -7,12 +7,21 @@ from advent21.utils import read_ints_sep_comma, handle_input, Timer
 
 
 class LanternFish:
-    def __init__(self, age: int) -> None:
-        self.just_hatched = False
-        self.age = age
+    # __slots__ = ["age", "school", "pass_one_day", "reproduce"]  # <-- allowed attributes
 
-    def pass_one_day(self, age):
-        self.age -= 1
+    def __init__(self, age: int, school: list) -> None:
+        self.age = age
+        self.school = school
+
+    def pass_one_day(self):
+        if self.age == 0:
+            self.age = 6
+            # self.reproduce()
+        else:
+            self.age -= 1
+
+    def reproduce(self):
+        self.school.append(LanternFish(8, self.school))
 
 
 if __name__ == "__main__":
@@ -21,6 +30,18 @@ if __name__ == "__main__":
 
     day_number = 6
     data_path = handle_input(day_number, sys.argv[1])
-    fish = read_ints_sep_comma(data_path)
+    fishes = read_ints_sep_comma(data_path)
+    school = []
+    for age in fishes:
+        school.append(LanternFish(age, school))
+
+    print([type(x) for x in school])
+    print([x.age for x in school])
+
+    for day in range(18):
+        school = [x.pass_one_day() for x in school if isinstance(x, LanternFish)]
+        print(len(school))
+        # fishes = [x.age for x in school]
+        # print(f"After {day} days:{fishes}")
 
     t.stop()
