404cf44218284d7f1b8ac9a0a1fdea4f5c7e6a04
James Trimarco <james.trimarco@gmail.com>
2021-12-06 20:51:17 -0500

    solves part 1

--- src/advent21/day_6/part_1.py
+++ src/advent21/day_6/part_1.py
@@ -6,22 +6,13 @@ from pathlib import Path
 from advent21.utils import read_ints_sep_comma, handle_input, Timer
 
 
-class LanternFish:
-    # __slots__ = ["age", "school", "pass_one_day", "reproduce"]  # <-- allowed attributes
-
-    def __init__(self, age: int, school: list) -> None:
-        self.age = age
-        self.school = school
-
-    def pass_one_day(self):
-        if self.age == 0:
-            self.age = 6
-            # self.reproduce()
-        else:
-            self.age -= 1
-
-    def reproduce(self):
-        self.school.append(LanternFish(8, self.school))
+def pass_one_day(fish: int):
+    if fish == 0:
+        fish = 6
+        school.append(9)
+    else:
+        fish -= 1
+    return fish
 
 
 if __name__ == "__main__":
@@ -32,16 +23,12 @@ if __name__ == "__main__":
     data_path = handle_input(day_number, sys.argv[1])
     fishes = read_ints_sep_comma(data_path)
     school = []
-    for age in fishes:
-        school.append(LanternFish(age, school))
 
-    print([type(x) for x in school])
-    print([x.age for x in school])
+    for fish in fishes:
+        school.append(fish)
 
-    for day in range(18):
-        school = [x.pass_one_day() for x in school if isinstance(x, LanternFish)]
+    for day in range(80):
+        school = [pass_one_day(x) for x in school if isinstance(fish, int)]
         print(len(school))
-        # fishes = [x.age for x in school]
-        # print(f"After {day} days:{fishes}")
 
     t.stop()
