e7336d44938eefddf8020f66de54542225827d83
James Trimarco <james.trimarco@gmail.com>
2021-12-06 23:04:49 -0500

    fastest approach so far

--- src/advent21/day_6/part_2.py
+++ src/advent21/day_6/part_2.py
@@ -21,23 +21,25 @@ if __name__ == "__main__":
     day_number = 6
     data_path = handle_input(day_number, sys.argv[1])
     fishes = read_ints_sep_comma(data_path)
-    school = []
+    school = np.array([], dtype=np.uint8)  # start with 100 million spaces in the array
+    print(school.shape)
 
     for index, fish in enumerate(fishes):
-        school.append(fish)
+        school = np.append(school, [fish], 0)
 
-    for day in range(180):
+    for day in range(256):
         print(day)
         t.start()
-        count_zeros = school.count(0)
-        t.stop()
-        t.start()
-        school = [*map(lambda x: 6 if x == 0 else x - 1, school)]
+        count_zeros = school.size - np.count_nonzero(school)
         t.stop()
 
         t.start()
-        school.extend([8] * count_zeros)
+        school = np.where(school == 0, 6, school - 1)
         t.stop()
 
-    print(len(school))
-    t.stop()
+        t.start()
+        school = np.concatenate([school, np.repeat(8, count_zeros)])
+        t.stop()
+
+    print(school.size)
+    # t.stop()
